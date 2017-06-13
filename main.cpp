119120937edab7e933ba01671d4e31288573e13c
Richard Užík <richard.uzik2@gmail.com>
2017-06-13 12:34:27 +0200

    Funkcia na divisors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-
+#include <vector>
+#include <NTL/GF2X.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+NTL_CLIENT
+
+vector<int> printDivisors(int n){
+    vector<int> v;
+	for (int i=1;i<=n;i++)
+        if (n%i==0)
+        	v.push_back(i);
+	return v;
+}
 
 int main(int argc, char** argv) {
+	
+	int n;
+	
+	cout << "Zadajte vstupne cislo n: ";
+	cin >> n;
+	cout << "Zadali ste : n = " << n << endl;
+	
+	vector<int> v = printDivisors(n);
+	for(vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+    	cout << *it << endl;
+	}
+	
+	system("pause");
 	return 0;
 }
